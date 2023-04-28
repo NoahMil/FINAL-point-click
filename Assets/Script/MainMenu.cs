f78ca82e2054426c64dae25cc730f60e4bea45d1
NoahMil <120110545+NoahMil@users.noreply.github.com>
2023-04-28 16:22:53 +0200

    start13

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    public void Start()
-    {
-    }
-
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
